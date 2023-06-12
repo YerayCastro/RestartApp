b7ee7e91e3c8eae26bb38eb563b7b065b6b6868e
YerayCastro <112658475+YerayCastro@users.noreply.github.com>
2023-06-12 14:41:24 +0200

    commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Yery Castro on 26/5/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
